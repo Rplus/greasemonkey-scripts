8abd484ea501246b9efe2ce4790d3d380e66dee9
Rplus <rplus.tw@gmail.com>
2015-07-11 12:15:36 +0800

    init pixiv hover zoom with greasemonkey script info

---
+++ Pixiv/pixiv-hover-zoom.js
@@ -0,0 +1,15 @@
+/* global window, document, console, jQuery */
+
+// ==UserScript==
+// @name         Pixiv hover zoom
+// @namespace    https://github.com/rplus
+// @version      0.3
+// @description  hover zoom for Pixiv
+// @author       Rplus
+// @include      http://www.pixiv.net/*
+// @grant        none
+// ==/UserScript==
+
+;(function(window, document, $) {
+
+})(window, document, jQuery, undefined);
